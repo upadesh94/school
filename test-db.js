e38bc6dbfb5860d6be63f9b11eb35cd6b58422f2
Upadesh Janjal <upadeshjanjal@gmail.com>
2026-07-30 07:59:44 +0530

    chore: clean up debugging scripts

--- test-db.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-require('./config/db');
-const mongoose = require('mongoose');
-const Admin = require('./models/Admin');
-setTimeout(async () => {
-  const admin = await Admin.findOne({ username: 'mukhyadhyapak' });
-  console.log('Admin:', admin);
-  process.exit(0);
-}, 2000);
--- test-fs2.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const Admin = require('./models/Admin');
-setTimeout(async () => {
-  const adminByEmail = await Admin.findOne({ email: 'principle@gmial.com' });
-  const adminByUsername = await Admin.findOne({ username: 'principle@gmial.com' });
-  console.log('By Email:', adminByEmail);
-  console.log('By Username:', adminByUsername);
-  process.exit(0);
-}, 2000);
